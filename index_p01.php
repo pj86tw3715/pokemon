5bc0cba3533c7208807d660ca0fa61388a20cc5e
JP <pj86tw@gmail.com>
2024-04-30 16:01:07 +0800

    pokemon

---
+++ index_p01.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="zh-TW">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>被告定了</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="website_p01.css">
+</head>
+
+<body>
+    <section id="header">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="./imagesen/tcg-logo.png" alt="寶可夢" class="img-fluid "></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">新手指南</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">商品資訊</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">寶可夢</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">PTCG</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                寶可夢歷代
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">第一世代 寶可夢 紅／綠</a></li>
+                                <li><a class="dropdown-item" href="#">第二世代 寶可夢 金／銀</a></li>
+                                <li><a class="dropdown-item" href="#">第三世代 寶可夢 紅寶石／藍寶石/綠寶石</a></li>
+                                <li><a class="dropdown-item" href="#">第四世代 寶可夢 黑／白</a></li>
+                                <li><a class="dropdown-item" href="#">第五世代 寶可夢 黑／白</a></li>
+                                <li><a class="dropdown-item" href="#">第六世代 寶可夢 X／Y</a></li>
+                                <li><a class="dropdown-item" href="#">第七世代 寶可夢 太陽／月亮</a></li>
+                                <li><a class="dropdown-item" href="#">第八世代 寶可夢 劍／盾</a></li>
+                                <li><a class="dropdown-item" href="#">第九世代 寶可夢 朱／紫</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                更多
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">如何玩</a></li>
+                                <li><a class="dropdown-item" href="#">最新消息</a></li>
+                                <li><a class="dropdown-item" href="#">買買買</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+    <section id="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <!-- <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button> -->
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="./imagesen/10001.jpg" class="d-block w-100" alt="波盪水">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <p>波盪水</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./imagesen/10002.jpg" class="d-block w-100" alt="鐵斑葉">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <p>鐵斑葉</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="./imagesen/10003.jpg" class="d-block w-100" alt="猛雷鼓">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <p>猛雷鼓</p>
+                                </div>
+                            </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="scontent"></section>
+    <section id="footer"></section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</body>
+
+</html>
